48131ae41a422d734ba997b032201e6a0d6029c1
Viktor Nonov <vnonov@gmail.com>
2016-05-28 19:37:03 -0700

    add couple different effects with balls

--- coding-math/trig02.js
+++ coding-math/trig02.js
@@ -7,27 +7,74 @@ window.onload = function() {
   var centerY = height * .5;
   var centerX = width * .5;
   var offset = height * .4;
-  var speed = 0.01;
+  var speed = 0.05;
   var angle = 0;
   var x = 0;
 
-  var baseRadius = 100;
+  var baseRadius = 0;
+  var baseAlpha;
 
+  bounceBallWithSin();
 
-  render();
+  function bounceBallWithSin() {
+    var y = centerY;
+    var x = centerX;
+    var magnification = 50;
+    context.clearRect(0, 0, width, height);
 
-  function render() {
-    var y = centerY + Math.tan(angle) * offset;
+    baseRadius = Math.abs(Math.sin(angle)) * magnification;
+    baseAlpha = Math.abs(Math.sin(angle));
+    context.fillStyle = "rgba(0,0,0, " + baseAlpha + ")";
+    context.beginPath();
+    context.arc(x, y, baseRadius, 0, 2 * Math.PI, false);
+    context.fill();
+
+    angle += speed;
+
+    requestAnimationFrame(bounceBallWithSin);
+  }
+
+  function bounceBallWithTangent() {
+    var y = centerY;
+    var x = centerX;
+    var magnification = 50;
+    context.clearRect(0, 0, width, height);
+
+    baseRadius = Math.abs(Math.tan(angle)) * magnification;
+    context.beginPath();
+    context.arc(x, y, baseRadius, 0, Math.PI * 2, false);
+    context.fill();
+
+    angle += speed;
+
+    requestAnimationFrame(bounceBallWithTangent);
+  }
+
+  function bounceBall() {
+    var y = centerY + Math.sin(angle) * offset;
+    var x = centerX;
     context.clearRect(0, 0, width, height);
 
     context.beginPath();
     context.arc(x, y, 10, 0, Math.PI * 2, false);
-
     context.fill();
 
     angle += speed;
-    x += 0.1;
 
-    requestAnimationFrame(render);
+    requestAnimationFrame(bounceBall);
+  }
+
+  function goInCircleWithSinAndCos() {
+    var y = centerY + Math.sin(angle) * offset;
+    var x = centerX + Math.cos(angle) * offset;
+    context.clearRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI * 2, false);
+    context.fill();
+
+    angle += speed;
+
+    requestAnimationFrame(goInCircleWithSinAndCos);
   }
 };
